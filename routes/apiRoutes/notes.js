6d064a9367f8878602a415d9700c056141864907
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-02-15 22:14:07 -0600

    fixed errors, get and post fully work

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,28 +1,27 @@
 const router = require('express').Router();
 const { createNewNote } = require('../../lib/notes');
-const { notes } = require('../../db/db.json');
+const { notesArray } = require('../../db/db.json');
 
 
-
+//Handle get call to return all notes
 router.get('/notes', (req, res) => {
-    let results = notes;
-    // if (req.query) {
-    //     results = filterByQuery(req.query, results);
-    // }
+  let results = notesArray;
   res.json(results);
   console.log("results", results)
 });
 
+//handle request to save a new note after validation
 router.post('/notes', (req, res) => {
 
   if (!validateNote(req.body)) {
     res.status(400).send('Both a title and body text are required!');
   } else {
-    const note = createNewNote(req.body, notes);
+    const note = createNewNote(req.body, notesArray);
     res.json(note);
   }
 });
 
+//validate that the note has a title and body
 const validateNote = (body) => {
     if (!body.title || !body.text ) {
         return false
